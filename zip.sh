15a93045af429de8997c21970743230e745444a1
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-10-05 13:53:27 +0100

    add zip script

---
+++ zip.sh
@@ -0,0 +1,8 @@
+mkdir project1_group25
+cp Ex1.dfy project1_group25
+cp Ex2.dfy project1_group25
+cp Ex3.dfy project1_group25
+cp Ex4.dfy project1_group25
+cp Ex5.dfy project1_group25
+zip -r project1_group25.zip project1_group25
+rm -r project1_group25
